475603e605b324ebaf52c9a5a2a47362dc193d2b
Ryan Kuhns <rnkuhns@gmail.com>
2022-06-24 14:01:17 -0400

    update registry package imports

--- base_object/registry/__init__.py
+++ base_object/registry/__init__.py
@@ -2,5 +2,7 @@
 """:mod:`base_object.registry` provides a registry of package functionality."""
 from typing import List
 
-__all__: List[str] = []
+from base_object.registry._lookup import all_objects, package_metadata
+
+__all__: List[str] = ["package_metadata", "all_objects"]
 __author__: List[str] = ["RNKuhns"]
